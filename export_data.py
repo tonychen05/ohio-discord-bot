8195e8d6ef42a1ee25986adeac3a4ab2ee01bf4a
Tony Chen <chent2828@gmail.com>
2026-03-09 22:56:06 -0400

    Updated export.py to work with new db schema

--- export_data.py
+++ export_data.py
@@ -1,99 +1,91 @@
 import csv
-import records
 import os
-import pandas as pd
-import discord
 
-#For all teams in the teamDB, get teamName and all users associated with it
+import pandas as pd
+
+import records
+
+# For all teams in the teamDB, get teamName and all users associated with it
 # TeamData = [Team ID, TeamName, Members...]
 
-EXPORT_FILENAME = 'team_export.csv'
+EXPORT_FILENAME = "team_export.csv"
+
 
 # ----------- Grab Team Data from DB ------------- #
 def get_team_data():
-    HIGHEST_TEAM_ID = records.get_max_team_id()
-    team_data_list = []
+    teams = sorted(records.get_all_teams(), key=lambda team: team["id"])
+    rows: list[dict] = []
+    max_members = 0
 
-    #Add Header to List
-    headers = ['Team ID', 'Team Name', 'Members...']
-    team_data_list.append(headers)
+    for team in teams:
+        team_members = records.get_team_members(team["id"])
+        member_emails = [member["email"] for member in team_members]
+        max_members = max(max_members, len(member_emails))
+        rows.append(
+            {
+                "team_id": team["id"],
+                "team_name": team["name"],
+                "member_emails": member_emails,
+            }
+        )
 
-    for team_id in range(HIGHEST_TEAM_ID):
-        team_data = []
-        #If the team exists
-        if (records.team_exists(team_id)):
-            #Store id and name
-            team_data.append(team_id)
-            team_data.append(records.get_team_name(team_id)) 
+    headers = ["Team ID", "Team Name"] + [
+        f"Member {index} Email" for index in range(1, max_members + 1)
+    ]
+    team_data_list = [headers]
 
-            #Look for members TODO: Turn member_id into discord username
-            team_members = records.get_team_members_emails(team_id)
-            for member in team_members:
-                #Clean Email
-                team_data.append(str(member)[2:-3])
-
-            #Append to data list≠
-            team_data_list.append(team_data)
+    for row in rows:
+        team_row = [row["team_id"], row["team_name"]]
+        member_values = row["member_emails"] + [""] * (
+            max_members - len(row["member_emails"])
+        )
+        team_data_list.append(team_row + member_values)
 
     return team_data_list
 
 
-
 # ----------- Export Data to CSV --------------- #
-def export_to_csv(EXPORT_FILENAME:str, data: list):
-    #Remove file if it exists so it can be overwritten
-    if os.path.isfile(EXPORT_FILENAME): os.remove(EXPORT_FILENAME)
+def export_to_csv(export_filename: str, data: list):
+    # Remove file if it exists so it can be overwritten
+    if os.path.isfile(export_filename):
+        os.remove(export_filename)
 
-    with open(EXPORT_FILENAME, 'w') as csv_file:
-        #Export Header Items
+    with open(export_filename, "w", newline="", encoding="utf-8") as csv_file:
+        # Export Header Items
         writer = csv.writer(csv_file)
 
-        #Add Rows of Data
+        # Add Rows of Data
         for row in data:
             writer.writerow(row)
-        
-        csv_file.close()
 
 
-def append_to_xlsx(EXPORT_FILENAME:str, sheets:list[str]):
-    if os.path.isfile(EXPORT_FILENAME): os.remove(EXPORT_FILENAME)
+def append_to_xlsx(export_filename: str, sheets: list[str]):
+    if os.path.isfile(export_filename):
+        os.remove(export_filename)
 
     # Create a new Excel writer object
-    with pd.ExcelWriter(EXPORT_FILENAME, engine='xlsxwriter', mode='w') as writer:
+    with pd.ExcelWriter(export_filename, engine="xlsxwriter", mode="w") as writer:
         # Write the CSV data to a new sheet
         for sheet in sheets:
-            #Import csv data as sheet to excel file
+            # Import csv data as sheet to excel file
             csv_file = pd.read_csv(sheet)
-            csv_file.to_excel(writer, sheet_name=sheet.replace('.csv',''), index=False)
+            csv_file.to_excel(writer, sheet_name=sheet.replace(".csv", ""), index=False)
 
-    #Remove CSV files
+    # Remove CSV files
     for file in sheets:
         os.remove(file)
 
 
-
-
-#Retrieve Data
+# Retrieve Data
 team_data_list = get_team_data()
 
 
-#Compile into CSV
+# Compile into CSV
 sheets = []
 
-export_to_csv('team_export.csv', team_data_list)
-sheets.append('team_export.csv')
+export_to_csv("team_export.csv", team_data_list)
+sheets.append("team_export.csv")
 
 
-#Compile into Excel
+# Compile into Excel
 # append_to_xlsx('Report.xlsx', sheets)
-
-
-
-
-
-
-
-
-
-
-
